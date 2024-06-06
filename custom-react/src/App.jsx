2d4cfb784b3944262897f4c53101c20c3f4ed08b
AkshandraSingh <nameste380@gmail.com>
2024-06-07 01:32:44 +0530

    Update: addingCode

---
+++ custom-react/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  //! Using variables in components ✨
+  const userPassword = "itsMyPassword";
+  return (
+    <>
+      <h1>This is my Custom React Js 🚀</h1>
+      <p>Node.Js Vs React.Js</p>
+      <p>Password of user : {userPassword}</p>
+    </>
+  );
+}
+
+export default App;
